1daa64d370fa746e53412d16e2b0959adfaf51a6
shivamkumar-nickelfox <shivam.kumar@nickelfox.com>
2022-09-06 16:20:06 +0530

    removed unwanted category

--- src/Data/catogery.js
+++ src/Data/catogery.js
@@ -1,14 +1,10 @@
 const catogeries=[
     'business',
     'entertainment',
-    'environment',
-    'food',
-    'health',
     'politics',
     'science',
     'sports',
     'technology',
     'top',
-    'world'
 ]
 export default catogeries;
